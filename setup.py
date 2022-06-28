46d41411345760902ddb926868dc3c840c8a94bf
Aadyot Bhatnagar <abhatnagar@salesforce.com>
2022-06-28 13:05:44 -0700

    Update prophet required version to 1.1.
    Prophet 1.1 removes the dependency on pystan. Since pystan installation
    is quite complicated, this change makes Merlion as a whole much simpler
    to install.

--- setup.py
+++ setup.py
@@ -13,12 +13,7 @@ MERLION_JARS = [
 ]
 
 # optional dependencies
-extra_require = {
-    "plot": ["plotly>=4.13"],
-    "prophet": ["prophet", "pystan<3.0"],  # pystan >= 3.0 doesn't work with prophet
-    "deep-learning": ["torch>=1.1.0"],
-    "spark": ["pyspark[sql]>=3"],
-}
+extra_require = {"plot": ["plotly>=4.13"], "deep-learning": ["torch>=1.1.0"], "spark": ["pyspark[sql]>=3"]}
 extra_require["all"] = sum(extra_require.values(), [])
 
 
@@ -52,6 +47,8 @@ setup(
         "numpy>=1.19; python_version < '3.7'",  # however, numpy 1.20+ requires python 3.7+
         "packaging",
         "pandas>=1.1.0",  # >=1.1.0 for origin kwarg to df.resample()
+        "prophet>=1.1; python_version >= '3.7'",  # 1.1 removes dependency on pystan
+        "prophet==1.0.1; python_version < '3.7'",  # however, prophet 1.1 requires python 3.7+
         "scikit-learn>=0.22",  # >=0.22 for changes to isolation forest algorithm
         "scipy>=1.6.0; python_version >= '3.7'",  # 1.6.0 adds multivariate_t density to scipy.stats
         "scipy>=1.5.0; python_version < '3.7'",  # however, scipy 1.6.0 requires python 3.7+
